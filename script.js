aad80f0e5119ba5af9733b1d85300d24610023a5
Edinei Abreu <62905166+EdineiAbreu@users.noreply.github.com>
2020-05-18 21:57:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function rgb_R() {
+  var slider = document.getElementById("sliderR").value;
+  document.getElementById("box-R").value = slider;
+}
+function rgb_G() {
+  var slider = document.getElementById("sliderG").value;
+  document.getElementById("box-G").value = slider;
+
+}
+function rgb_B() {
+  var slider = document.getElementById("sliderB").value;
+  document.getElementById("box-B").value = slider;
+  }
